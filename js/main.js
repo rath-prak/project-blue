26a2c23ef17abb4fddfe40ddfc25397d1f5a2795
rath-prak <rathanap@gmail.com>
2016-07-26 03:42:59 +1200

    add slides section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
 
+	// ScrollMagic code will go here
+	var slide00 = $('#slide00');
+	var slide01 = $('#slide01');
+
+
+	TweenMax.to(slide00, 2, {autoAlpha: 0})
+	TweenMax.to(slide01, 2, {autoAlpha: 1})
+
+});
