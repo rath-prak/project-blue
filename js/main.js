204737ae8a18dea985bf5a41dcb49d2b05d9d4e7
rath-prak <rathanap@gmail.com>
2016-07-26 16:03:21 +1200

    add slides

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ $(document).ready(function(){
 	// ScrollMagic code will go here
 	var slide00 = $('#slide00');
 	var slide01 = $('#slide01');
+	var logo = $('#logo');
+
+	tl = new TimelineMax();
+
+	tl.to(slide00, 2, {autoAlpha: 0})
+		.to(slide01, 2, {autoAlpha: 1})
+		.fromTo(logo, 1, {css: {scale: 0}}, {css: {scale: 3}, ease: Bounce.easeOut }, '-=1')
 
 
-	TweenMax.to(slide00, 2, {autoAlpha: 0})
-	TweenMax.to(slide01, 2, {autoAlpha: 1})
+
+
+
 
 });
