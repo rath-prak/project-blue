a0ed1c1e38324d72b26669578ad88473352929ef
rath-prak <rathanap@gmail.com>
2016-07-26 18:00:33 +1200

    html structure

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
 	// ScrollMagic code will go here
-	var slide00 = $('#slide00');
 	var slide01 = $('#slide01');
+	var slide02 = $('#slide02');
 	var logo = $('#logo');
 
 	tl = new TimelineMax();
 
-	tl.to(slide00, 2, {autoAlpha: 0})
-		.to(slide01, 2, {autoAlpha: 1})
-		.fromTo(logo, 1, {css: {scale: 0}}, {css: {scale: 3}, ease: Bounce.easeOut }, '-=1')
+	tl.to(slide01, 2, {autoAlpha: 0})
+		.to(slide02, 2, {autoAlpha: 1})
+		.fromTo(logo, 1, {css: {scale: 0}}, {css: {scale: 3}, ease: Bounce.easeOut }, '-=1');
 
 
 
