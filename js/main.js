ef287e76b60f1e67ee94eb310ae6785c13d2edde
rath-prak <rathanap@gmail.com>
2016-07-28 00:13:11 +1200

    started on new layout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 
+	// setting photographySection tween
+	var photo = $('#photo'),
+		photoText = $('#photoText');
 
+	// tl.set(photo, {autoAlpha: 0})
+	var photographySection = TweenMax.to(photo, 2, {
+		y: '50px'
+	})
 
+	var controller = new ScrollMagic.Controller();
 
+	slide02Scene = new ScrollMagic.Scene({
+		triggerElement: '#slide02',
+		triggerHook: .3,
+		duration: '50%'
+	})
+	.setTween(photographySection)
+	.addIndicators()
+	.addTo(controller);
 
 
 }); // end of document.ready
